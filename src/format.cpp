ab6545981971dc8f9af4e783ee54683fe8ad94f2
issaiass <issaiass@hotmail.com>
2022-04-15 15:57:11 +0000

    format time working, not poped up, just displaying 00:00:00

--- src/format.cpp
+++ src/format.cpp
@@ -2,10 +2,33 @@
 
 #include "format.h"
 
-using std::string;
-
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+using std::to_string;
+using std::string;
+
+string Format::ElapsedTime(long seconds) { 
+    int hr = 0;
+    int min = 0;
+    int sec = 0;
+    string t;
+    string h, m, s;
+    
+    hr = seconds / 3600;
+    min = (seconds % 3600) / 60;
+    sec = seconds % 60;
+
+    h = to_string(hr);
+    m = to_string(min);
+    s = to_string(sec);
+
+    h = hr  < 10 ? "0" + h : h;    
+    m = min < 10 ? "0" + m : m;    
+    s = sec < 10 ? "0" + s : s;
+    
+    t = h + ":" + m + ":" + s;
+
+    return t;
+}
